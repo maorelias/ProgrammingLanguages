df4a4edce69f5beb4c79715414b31710677050ed
sofiarov <srsoniarov@gmail.com>
2016-05-25 14:58:11 +0300

    question 9

--- pl16_ex4/parser.ml
+++ pl16_ex4/parser.ml
@@ -27,13 +27,13 @@ exception SyntaxError of string
 
 let rec parse_term ts =
   match ts with
-    | [] -> raise (SyntaxError "Term expected\n")
+    | [] -> raise (SyntaxError "Term expected")
     | (Literal id) :: ts' -> (Variable id), ts'
     | LParen :: (LambdaTok :: ((Literal id) :: (DotTok :: ts')))  -> 
 			let t1, ts'' = parse_term ts' in (
 			  match ts'' with
 			  | RParen :: ts''' -> Abstraction(id, t1), ts'''
-			  | _ -> raise (SyntaxError "Unexpected token\n")
+			  | _ -> raise (SyntaxError "Unexpected token")
 			)
     | LParen :: ts' ->  	    
 		    let t1, ts'' =  parse_term ts' in (
@@ -42,7 +42,7 @@ let rec parse_term ts =
 			    | _ -> let t2, ts''' = parse_term ts'' in (
 			    match ts''' with
 				  | RParen :: ts'''' -> (Application (t1, t2)), ts''''
-				  | _ -> raise (SyntaxError "Unexpected token. RParen expected\n")
+				  | _ -> raise (SyntaxError "Unexpected token. RParen expected")
 		      )
 			)
     | LetTok :: (Literal id :: (EqTok :: ts')) -> let t1, ts'' = parse_term ts' in (
@@ -50,9 +50,9 @@ let rec parse_term ts =
 	    | InTok :: ts''' -> let t2, ts'''' = parse_term ts''' in (
 	      (Application ((Abstraction (id, t2)), t1)), ts'''' 
 	    )
-	    | _ -> raise (SyntaxError "Unexpected token. InTok expected\n")
+	    | _ -> raise (SyntaxError "Unexpected token. InTok expected")
 	)
-	| _ -> raise (SyntaxError "Parsing error. Unexpected token\n")
+	| _ -> raise (SyntaxError "Parsing error. Unexpected token")
 
 
 let rec format_term = function
@@ -66,12 +66,141 @@ let parse s =
 	      		in
 	      		match ts with
 	      		| [] -> t1
-	      		| _ -> raise (SyntaxError "Unexpected input.\n")
+	      		| _ -> raise (SyntaxError "Unexpected input.")
 	
 		
 let rec format_term_conv = function
   | Variable var ->  var 
   | Abstraction(s,t1) -> "\\" ^ s ^ "." ^ (format_term_conv t1)
-  | Application(t1, t2) -> (format_term_conv t1) ^ " " ^ (format_term_conv t2) 		
+  | Application(t1, t2) -> (format_term_conv t1) ^ " " ^ (format_term_conv t2) 	
+  
+let get_first lst =
+	match lst with
+	| first :: rest -> first
+	| [] -> raise (SyntaxError "Empty list")
+
+let rec my_parse_term_conv1 ts =
+  match ts with
+    | [] -> raise (SyntaxError "Term expected")
+    | (Literal id) :: ts' -> (Variable id), ts'
+    | LParen :: (LambdaTok :: ((Literal id) :: (DotTok :: ts')))  -> 
+			let t1, ts'' = my_parse_term_conv1 ts' in (
+			  match ts'' with
+			  | RParen :: ts''' -> Abstraction(id, t1), ts'''
+			  | _ -> raise (SyntaxError "Unexpected token")
+			)
+    | LParen :: ts' ->  	    
+		    let t1, ts'' =  my_parse_term_conv1 ts' in (
+			  match ts'' with
+			    | RParen :: ts''' -> t1, ts'''
+			    | _ -> let ref_t1, ref_ts'' = ref t1, ref ts'' in (
+					while ((!ref_ts'' <> []) && ((get_first !ref_ts'') <> RParen)) do 
+						let t2, ts''' = my_parse_term_conv1 !ref_ts'' in (
+							ref_t1 := Application(!ref_t1, t2);
+							ref_ts'' := ts''';
+					)	
+				   done;
+				   match !ref_ts'' with
+				   | RParen :: lst -> !ref_t1,lst
+				   | _ -> raise (SyntaxError "Unexpected token. RParen expected")
+				)
+			)
+    | LetTok :: (Literal id :: (EqTok :: ts')) -> let t1, ts'' = my_parse_term_conv1 ts' in (
+	  match ts'' with
+	    | InTok :: ts''' -> let t2, ts'''' = my_parse_term_conv1 ts''' in (
+	      (Application ((Abstraction (id, t2)), t1)), ts''''
+	    )
+	    | _ -> raise (SyntaxError "Unexpected token. InTok expected")
+	)
+    | LambdaTok :: ((Literal id) :: (DotTok :: ts'))  -> 
+			let t1, ts'' = my_parse_term_conv1 ts' in (
+			  match ts'' with
+			  | [] -> Abstraction(id, t1), []
+			  | RParen :: ts''' -> Abstraction(id, t1), ts''
+			  | _ -> let ref_t1, ref_ts'' = ref t1, ref ts'' in (
+				while ((!ref_ts'' <> []) && ((get_first !ref_ts'') <> RParen)) do 
+					let t2, ts''' = my_parse_term_conv1 !ref_ts'' in (
+						ref_t1 := Application(!ref_t1, t2);
+						ref_ts'' := ts''';
+				)	
+			   done;
+			   Abstraction(id, !ref_t1), !ref_ts''
+			))
+    | _ ->  	    
+		    let t1, ts' =  my_parse_term_conv1 ts in (
+			  match ts' with
+			    | [] -> t1, ts'
+			    | _ -> let t2, ts'' = parse_term ts' in (
+					(Application (t1, t2)), ts''
+		        )
+			)
+			
+let rec my_parse_term_conv (t, lst) = 
+	let ref_t1, ref_lst = ref t, ref lst in (
+				while ((!ref_lst <> []) && ((get_first !ref_lst) <> RParen)) do 
+					let t2, lst'' = my_parse_term_conv1 !ref_lst in (
+						ref_t1 := Application(!ref_t1, t2);
+						ref_lst := lst'';
+				)	
+			   done;
+			   !ref_t1, !ref_lst
+			)
+
+let rec parse_term_conv ts =
+  match ts with
+    | [] -> raise (SyntaxError "Term expected.")
+    | (Literal id) :: ts' -> my_parse_term_conv((Variable id), ts')
+    | LParen :: (LambdaTok :: ((Literal id) :: (DotTok :: ts')))  -> 
+			let t1, ts'' = my_parse_term_conv1 ts' in (
+			  match ts'' with
+			  | RParen :: ts''' -> my_parse_term_conv (Abstraction(id, t1), ts''')
+			  | _ -> raise (SyntaxError "Unexpected token")
+			)
+    | LParen :: ts' ->  	    
+		    let t1, ts'' =  my_parse_term_conv1 ts' in (
+			  match ts'' with
+			    | RParen :: ts''' -> my_parse_term_conv(t1, ts''')
+			    | _ -> let ref_t1, ref_ts'' = ref t1, ref ts'' in (
+					while ((!ref_ts'' <> []) && ((get_first !ref_ts'') <> RParen)) do 
+						let t2, ts''' = my_parse_term_conv1 !ref_ts'' in (
+							ref_t1 := Application(!ref_t1, t2);
+							ref_ts'' := ts''';
+					)	
+				   done;
+				   match !ref_ts'' with
+				   | RParen :: lst-> my_parse_term_conv (!ref_t1, lst)
+				   | _ -> raise (SyntaxError "Unexpected token. RParen expected")
+				)
+			)
+    | LetTok :: (Literal id :: (EqTok :: ts')) -> let t1, ts'' = my_parse_term_conv1 ts' in (
+	  match ts'' with
+	    | InTok :: ts''' -> let t2, ts'''' = my_parse_term_conv1 ts''' in (
+	      my_parse_term_conv((Application ((Abstraction (id, t2)), t1)), ts'''')
+	    )
+	    | _ -> raise (SyntaxError "Unexpected token. InTok expected")
+	)
+    | LambdaTok :: ((Literal id) :: (DotTok :: ts'))  -> 
+			let t1, ts'' = my_parse_term_conv1 ts' in (
+			  match ts'' with
+			  | [] -> Abstraction(id, t1), []
+			  | RParen :: ts''' -> Abstraction(id, t1), ts''
+			  | _ -> let ref_t1, ref_ts'' = ref t1, ref ts'' in (
+				while ((!ref_ts'' <> []) && ((get_first !ref_ts'') <> RParen)) do 
+					let t2, ts''' = my_parse_term_conv1 !ref_ts'' in (
+						ref_t1 := Application(!ref_t1, t2);
+						ref_ts'' := ts''';
+				)	
+			   done;
+			   my_parse_term_conv(Abstraction(id, !ref_t1), !ref_ts'')
+			))
+    | _ -> raise (SyntaxError "Unexpected token. RParen expected")
+		
+let parse_conv s = 
+                let (t1, ts) =
+				   s |> string_to_list |> tokenize |> parse_term_conv
+	      		in
+	      		match ts with
+	      		| [] -> t1
+	      		| _ -> raise (SyntaxError "Unexpected input in parse_conv.")
 		
 		
